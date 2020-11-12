cbebdb7b9b369916f02b71892cf58b382a455b2e
Tim Crone <tjcrone@gmail.com>
2020-11-12 15:08:58 -0500

    Split root and user scripts

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 
 ## Beaglebone setup
-```
-wget https://raw.githubusercontent.com/tjcrone/nutbox-public/main/beaglebone/setup.sh;sudo /bin/bash setup.sh
-```
+1) `wget https://raw.githubusercontent.com/tjcrone/nutbox-public/main/beaglebone/setup.sh;sudo /bin/bash setup.sh`
+2) `wget https://raw.githubusercontent.com/tjcrone/nutbox-public/main/beaglebone/setup_user.sh;/bin/bash setup_user.sh
+`
---
+++ beaglebone/setup_user.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Setup script for the Nutbox Beaglebone. User-space components
+# This script works with: bone-eMMC-flasher-debian-10.5-console-armhf-2020-08-25-1gb.img
+
+set -e
+
+# locations
+HOME="/home/debian"
+NUTBOX="$HOME/nutbox"
+
+# move to user home
+echo "Setting up Beaglebone ..."
+cd $HOME
+
+# install deploy key
+rm -rf ".ssh"
+curl -L -o "deploy.tar.gpg" "https://github.com/tjcrone/nutbox-public/blob/main/beaglebone/deploy.tar.gpg?raw=true"
+
+decrypt_file () {
+    gpg --no-symkey-cache -d "deploy.tar.gpg" > "deploy.tar"
+}
+
+while ! decrypt_file; do
+    echo "Incorrect password. Try again."
+done
+
+tar -xvf "deploy.tar"
+rm "deploy.tar"
+rm "deploy.tar.gpg"
+
+# clone the nutbox private repo
+rm -rf nutbox
+git clone "git@github.com:tjcrone/nutbox.git"
+
+# symlink dotfiles
+mv -f .bashrc .bashrc.bak0
+ln -s -f nutbox/beaglebone/.bashrc
+ln -s -f nutbox/beaglebone/.gitconfig
+
+# venv
+export PIP_NO_CACHE_DIR="off"
+python -m venv venv
+source venv/bin/activate
+pip install -r nutbox/beaglebone/requirements.txt
+
+# report status
+echo "Beaglebone setup complete."
+
+# instructions
+echo "Run Jupyterlab with:"
+echo "jupyter lab --no-browser --port=5678"
+
+# scratch
+# screen
